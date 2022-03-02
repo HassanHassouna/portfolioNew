481379f97826c5d297e1b4ebdea79993b6ba124c
hhasona <hhasona@gmail.com>
2022-03-02 13:48:45 +0200

    complete about me section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,7 +1,50 @@
 import React from "react"
 import "./about.css"
+import ME from "../../assets/me2.jpeg"
+import { FaAward, FaCode, FaLaptopCode, FaUser } from "react-icons/fa"
 const About = () => {
-  return <section id="about">About</section>
+  return (
+    <section id="about">
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ME} alt="me" />
+          </div>
+        </div>
+        <div className="about__content">
+          <div className="about__cards">
+            <article className="about__card">
+              <FaAward className="about__icon" />
+              <h5>Experience</h5>
+              <small>I am a Full Stack Developer</small>
+            </article>
+            <article className="about__card">
+              <FaAward className="about__icon" />
+              <h5>Experience</h5>
+              <small>I am a Full Stack Developer</small>
+            </article>
+            <article className="about__card">
+              <FaAward className="about__icon" />
+              <h5>Experience</h5>
+              <small>I am a Full Stack Developer</small>
+            </article>
+          </div>
+
+          <p>
+            lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          </p>
+
+          <a href="#contact" className="btn btn-primary">
+            Let's Talk
+          </a>
+        </div>
+      </div>
+    </section>
+  )
 }
 
 export default About
