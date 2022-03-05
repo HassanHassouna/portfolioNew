f5d1b3a4ff342a952a0073bb5e40fdb016ee314c
hhasona <hhasona@gmail.com>
2022-03-05 14:54:20 +0200

    tesimonials completed

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,6 +1,37 @@
 import React from "react"
 import "./testimonials.css"
+import data from "./data"
+import { Pagination } from "swiper"
+import { Swiper, SwiperSlide } from "swiper/react"
+import "swiper/css"
+import "swiper/css/pagination"
 const Testimonials = () => {
-  return <div>Testimonials</div>
+  return (
+    <section id="testimonials">
+      <h5>Review from people</h5>
+      <h2>Testimonials</h2>
+
+      <Swiper
+        className="container testimonials__container"
+        modules={[Pagination]}
+        spaceBetween={40}
+        slidesPerView={1}
+        pagination={{ clickable: true }}
+     
+      >
+        {data.map(({ id, name, review, avatar }) => {
+          return (
+            <SwiperSlide key={id} className="testemonials">
+              <div className="avatar">
+                <img src={avatar} alt="" />
+              </div>
+              <h5 className="name">{name}</h5>
+              <small className="review">{review}</small>
+            </SwiperSlide>
+          )
+        })}
+      </Swiper>
+    </section>
+  )
 }
 export default Testimonials
---
+++ src/components/testimonials/data.jsx
@@ -0,0 +1,19 @@
+import Avatar1 from "../../assets/karyum.jpeg"
+const data = [
+  {
+    id: 1,
+    name: "Mario Saliba - Team Lead",
+    review:
+      "Fun to hang out with, highly motivated to reach his goals, and a massive foodie!",
+    avatar: Avatar1,
+  },
+  {
+    id: 2,
+    name: "Mario Saliba - Team Lead",
+    review:
+      "Fun to hang out with, highly motivated to reach his goals, and a massive foodie!",
+    avatar: Avatar1,
+  },
+]
+
+export default data
