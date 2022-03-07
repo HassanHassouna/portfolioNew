7055d0db9b2e2d2776e7bba790a0810a01fd7031
hhasona <hhasona@gmail.com>
2022-03-07 11:32:00 +0200

    contact complete

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,7 +1,71 @@
-import React from "react"
+import React, { useRef } from "react"
 import "./contact.css"
+import { MdOutlineEmail } from "react-icons/md"
+import { BsWhatsapp } from "react-icons/bs"
+import emailjs from "emailjs-com"
+
 const Contact = () => {
-  return <section id="contact">Contact</section>
+  const form = useRef()
+
+  const sendEmail = (e) => {
+    e.preventDefault()
+
+    emailjs.sendForm(
+      "service_0j2ma3b",
+      "template_ID",
+      form.current,
+      "user_x9TAIhS4ZaZpYeGWrxTT9"
+    )
+    e.target.reset()
+  }
+  return (
+    <section id="contact">
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <MdOutlineEmail className="contact__option-icon" />
+            <h4>Email</h4>
+            <h5>hhasona@gmail.com</h5>
+            <a target="_blank" href="mailto:hhasona@gmail.com">
+              Send a message
+            </a>
+          </article>
+          <article className="contact__option">
+            <BsWhatsapp className="contact__option-icon" />
+            <h4>Whatsapp</h4>
+            <h5>+972533332942</h5>
+            <a
+              target="_blank"
+              href="https://api.whatsapp.com/send?phone=+972533332942"
+            >
+              Send a message
+            </a>
+          </article>
+        </div>
+        <form ref={form} onSubmit={sendEmail} action="">
+          <input
+            type="text"
+            placeholder="Your Full Name"
+            required
+            name="name"
+          />
+          <input type="email" placeholder="Your Email" required name="email" />
+          <textarea
+            placeholder="Your Message"
+            required
+            name="message"
+            rows="7"
+          ></textarea>
+          <button type="submit" className="btn btn-primary">
+            Send Message
+          </button>
+        </form>
+      </div>
+    </section>
+  )
 }
 
 export default Contact
--- src/components/testimonials/data.jsx
+++ src/components/testimonials/data.jsx
@@ -1,4 +1,5 @@
 import Avatar1 from "../../assets/karyum.jpeg"
+import Avatar2 from "../../assets/mahamed.jpeg"
 const data = [
   {
     id: 1,
@@ -9,10 +10,10 @@ const data = [
   },
   {
     id: 2,
-    name: "Mario Saliba - Team Lead",
+    name: "Mahmoud Mahamed - Mentor",
     review:
       "Fun to hang out with, highly motivated to reach his goals, and a massive foodie!",
-    avatar: Avatar1,
+    avatar: Avatar2,
   },
 ]
 
